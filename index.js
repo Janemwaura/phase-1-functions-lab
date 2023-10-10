f62ab7b889dbae3346272099f0455eb5b49243ff
Janemwaura <mwaura461@gmail.com>
2023-10-10 06:14:52 +0300

    Halfly Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+function distanceFromHQInBlocks (pickUp){
+    const hq = 42;
+    const blocks = math.abs(hq - pickUp);
+    return blocks;
+}
+console.log("distanceFromHQInBlocks");
+
+function distanceFromHQInFeet(pickUp){
+    return distanceFromHQInBlocks(pickUp) * 264;
+}
+console.log("distanceFromHQInFeet");
+
+
+
