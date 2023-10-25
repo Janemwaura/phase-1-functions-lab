ed04db8a7e87a6766b331ef8bc4811d099e020b5
Janemwaura <mwaura461@gmail.com>
2023-10-25 15:55:05 +0300

    Re-submission

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-function distanceFromHQInBlocks (pickUp){
-    const hq = 42;
-    const blocks = math.abs(hq - pickUp);
-    return blocks;
+function distanceFromHqInBlocks (pickup){
+ const hq = 42;
+ const blocks = Math.abs(hq - pickup);
+ return blocks;
+
 }
-console.log("distanceFromHQInBlocks");
-
-function distanceFromHQInFeet(pickUp){
-    return distanceFromHQInBlocks(pickUp) * 264;
+function distanceFromHqInFeet(pickup){
+    return distanceFromHqInBlocks(pickup) *264;
 }
-console.log("distanceFromHQInFeet");
+
+function  distanceTravelledInFeet(start,destination){
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start,destination){
+    const chargable = distanceTravelledInFeet(start,destination) - 400;
+    if (distanceTravelledInFeet(start,destination <= 400){
+        return 0;
+    }else if (distanceTravelledInFeet(start,destination) > 400 && distanceTravelledInFeet(start,destination) <= 2000) {
+        return (distanceTravelledInFeet(start,destination) - 400) * 0.02;
 
 
-
+} else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) <= 2500) {
+    return 25;
+} else {
+    return "cannot travel that far";
+}
+}
